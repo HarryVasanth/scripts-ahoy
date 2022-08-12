2eb63193320d7dd8c69c5fc69f3f4c08c68779fc
HarryVasanth <mailsuthan@gmail.com>
2022-08-12 00:26:50 +0000

    Prettified Code!

--- proxmox/pbs/pbs2-post-install.sh
+++ proxmox/pbs/pbs2-post-install.sh
@@ -3,17 +3,17 @@
 echo -e "\e[1;33m This script will Disable the Enterprise Repos, Add & Enable the No-Subscription Repos. \e[0m"
 
 while true; do
-    read -p "Do you wish to start the PBS 2 Post-Installation Script (y/n)?" yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer (y)es or (n)o.";;
-    esac
+  read -p "Do you wish to start the PBS 2 Post-Installation Script (y/n)?" yn
+  case $yn in
+    [Yy]*) break ;;
+    [Nn]*) exit ;;
+    *) echo "Please answer (y)es or (n)o." ;;
+  esac
 done
 
 sed -i "s/^deb/#deb/g" /etc/apt/sources.list.d/pbs-enterprise.list
 
-cat <<EOF > /etc/apt/sources.list
+cat << EOF > /etc/apt/sources.list
 deb http://ftp.debian.org/debian bullseye main contrib
 deb http://ftp.debian.org/debian bullseye-updates main contrib
 deb http://security.debian.org/debian-security bullseye-security main contrib
--- proxmox/pve/pve7-post-install.sh
+++ proxmox/pve/pve7-post-install.sh
@@ -3,17 +3,17 @@
 echo -e "\e[1;33m This script will Disable the Enterprise Repos, Add & Enable the No-Subscription Repos and stops it from nagging. \e[0m"
 
 while true; do
-    read -p "Do you wish to start the PVE 7 Post-Installation Script (y/n)?" yn
-    case $yn in
+  read -p "Do you wish to start the PVE 7 Post-Installation Script (y/n)?" yn
+  case $yn in
     [Yy]*) break ;;
     [Nn]*) exit ;;
     *) echo "Please answer (y)es or (n)o." ;;
-    esac
+  esac
 done
 
 sed -i "s/^deb/#deb/g" /etc/apt/sources.list.d/pve-enterprise.list
 
-cat <<EOF >/etc/apt/sources.list
+cat << EOF > /etc/apt/sources.list
 deb http://ftp.debian.org/debian bullseye main contrib
 deb http://ftp.debian.org/debian bullseye-updates main contrib
 deb http://security.debian.org/debian-security bullseye-security main contrib
@@ -23,7 +23,7 @@ EOF
 sed -i.backup -z "s/res === null || res === undefined || \!res || res\n\t\t\t.data.status.toLowerCase() \!== 'active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
 
 echo -e "\e[1;33m Repos updated. ProxmoxVE will update and upgrades itself now. \e[0m"
-systemctl restart pveproxy.service 
+systemctl restart pveproxy.service
 
 apt update && apt full-upgrade -y && apt autoremove -y && apt autoclean
 
